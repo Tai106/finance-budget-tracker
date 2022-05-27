1839ab929f3c74915c5918f42d45f1fe2cf33f59
Gravity <tevoned@gmail.com>
2022-05-27 17:03:37 -0400

    deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,83 +1,67 @@
-const CACHE_NAME = 'budget-tracker-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
-
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./manifest.json",
-    "./css/styles.css",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png",
-    "./js/idb.js",
-    "./js/index.js"
+  './index.html',
+  './js/index.js',
+  './js/idb.js',
+  './manifest.json',
+  './icons/icon-512x512.png',
+  './icons/icon-384x384.png',
+  './icons/icon-192x192.png',
+  './icons/icon-152x152.png',
+  './icons/icon-144x144.png',
+  './icons/icon-128x128.png',
+  './icons/icon-96x96.png',
+  './icons/icon-72x72.png'
 ];
 
-self.addEventListener('fetch', function (evt) {
-    if (evt.request.url.includes('/api/')) {
-        evt.respondWith(
-            caches
-                .open(DATA_CACHE_NAME)
-                .then(cache => {
-                    return fetch(evt.request)
-                        .then(response => {
-                        
-                            if (response.status === 200) {
-                                cache.put(evt.request.url, response.clone());
-                            }
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-                            return response;
-                        })
-                        .catch(err => {
-                        
-                            return cache.match(evt.request);
-                        });
-                })
-                .catch(err => console.log(err))
-        );
-
-        return;
-    }
-
-    evt.respondWith(
-        fetch(evt.request).catch(function () {
-            return caches.match(evt.request).then(function (response) {
-                if (response) {
-                    return response;
-                } else if (evt.request.headers.get('accept').includes('text/html')) {
-                    return caches.match('/');
-                }
-            });
-        })
-    );
-});
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('Your files were pre-cached successfully!')
-            return cache.addAll(FILES_TO_CACHE)
-        })
-    )
+    
+    })
+  )
 })
 
-self.addEventListener('activate', function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log('Removing old cache data', key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
 
-    self.clients.claim();
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      })
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] );
+          return caches.delete(keyList[i]);
+        }
+      }));
+    })
+  );
 });
